51b5814dc7bd12e418a8154418b403754b7f13d9
Kang Sunghyuk <latias0505@naver.com>
2023-05-09 14:09:31 +0900

    오후 수업 part1 & 과제 2 : 1~100 사이의 숫자만 입력받기

--- js/utils.js
+++ js/utils.js
@@ -11,3 +11,23 @@ function sum(x, y) {
 function total(x, y) {
     return x + y;
 }
+
+function asknum() {
+    let num = prompt("1에서 100사이의 숫자를 입력해주세요");
+    checknum(num);
+}
+
+function checknum(num) {
+    if(isNaN(num)) {
+        alert("숫자만 입력해주세요");
+        asknum();
+    } else if(num > 100 || num < 1) {
+        alert("1에서 100사이의 숫자만 입력해주세요");
+        asknum();
+    } else {
+        for(let i = 1; i <= 100; i++) {
+            console.log(i);
+        }
+        alert("완료되었습니다.");
+    }
+}
